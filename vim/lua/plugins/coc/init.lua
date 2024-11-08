2e8ed108b7a0f8ceb88016ca6e9364d9b21bbf3a
Justin.Y.Sun <Justin.Y.Sun@newegg.com>
2024-11-08 16:40:57 +0800

    remove comments

--- vim/lua/plugins/coc/init.lua
+++ vim/lua/plugins/coc/init.lua
@@ -5,30 +5,11 @@ return {
             return vim.g.usecoc
         end,
         dependencies = {
-            -- {
-            --     "williamboman/mason.nvim",
-            --     config = true,
-            -- },
-            -- {
-            --     "fannheyward/telescope-coc.nvim",
-            --     config = function()
-            --         require("telescope").setup({
-            --             extensions = {
-            --                 coc = {
-            --                     prefer_locations = false,
-            --                     push_cursor_on_edit = true,
-            --                     timeout = 3000,
-            --                 },
-            --             },
-            --         })
-            --         require("telescope").load_extension("coc")
-            --     end,
-            -- },
         },
         branch = "release",
         build = "npm ci",
         opts = {},
-        config = function()
+        config = function(opts)
             require("conf.coc").setup(opts)
         end,
     },
